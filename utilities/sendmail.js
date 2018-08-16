d67acea191fa3195cf8c6b4432f5e9f92fda2770
alonzathompson <thompson0005@gmail.com>
2018-08-16 18:10:14 -0400

    Fixing heroku

--- utilities/sendmail.js
+++ utilities/sendmail.js
@@ -1,5 +1,5 @@
 const nodemailer = require('nodemailer');
-const config = require('../config.js');
+const config = require('../config');
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
